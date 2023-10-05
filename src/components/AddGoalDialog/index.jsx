552cf4e4922d2abb1e1c1e103a8f2f4ec3f1f91e
void-ness <78547746+void-ness@users.noreply.github.com>
2023-10-06 00:21:23 +0530

    update: add new goals

--- src/components/AddGoalDialog/index.jsx
+++ src/components/AddGoalDialog/index.jsx
@@ -1,28 +1,50 @@
 import React, { useState } from "react";
 import Dialog from "@mui/material/Dialog";
 
-const AddGoalDialog = ({ onClose, selectedValue, open }) => {
-    const [userInput, setUserInput] = useState(selectedValue);
+const AddGoalDialog = ({ onClose, open }) => {
+    const [userGoal, setuserGoal] = useState({ desc: "", checked: false });
 
     const handleClose = () => {
-        onClose(selectedValue);
-        setUserInput(selectedValue);
+        // user has either not added the desc        
+        if (userGoal.desc === "") {
+            onClose(null);
+            return;
+        }
+
+        // or forgot posting it
+        else {
+            let confirmation = window.confirm("You haven't posted your goal. Do you want to go back without posting?");
+            if (confirmation) {
+                onClose(null);
+                setuserGoal({ ...userGoal, desc: "" });
+                return;
+            }
+        }
     }
 
-    const handleItemSelect = (value) => {
-        onClose(value);
+    const handleUpdateGoal = () => {
+        // user is trying to post a blank goal        
+        if (userGoal.desc === "") {
+            alert("Please enter a valid desc");
+            return;
+        }
+
+        else {
+            onClose(userGoal);
+            setuserGoal({ ...userGoal, desc: "" });
+        }
     }
 
     return (
         <Dialog open={open} onClose={handleClose}>
             <div className="userGoalInputContainer flex-col flex items-center px-6 py-8 bg-gradient-to-r from-neutral-400/80 to-neutral-900/90">
                 <div className="userInput grid grid-rows-2 mb-12 w-full">
-                    <label htmlFor="username" className="text-2xl mb-2">Update Goal</label>
+                    <label htmlFor="username" className="text-2xl mb-2">Add Goal</label>
 
-                    <input type="text" value={userInput} onChange={(e) => setUserInput(e.target.value)} placeholder="Enter your updated goal" id="username" className="outline-none rounded-md bg-slate-100 text-black px-3 w-96 text-xl transition-all ease-in" />
+                    <input type="text" value={userGoal.desc} onChange={(e) => setuserGoal({ ...userGoal, desc: e.target.value })} placeholder="Enter your new goal" id="username" className="outline-none rounded-md bg-slate-100 text-black px-3 w-96 text-xl transition-all ease-in" />
                 </div>
 
-                <button className="text-black bg-slate-300 hover:bg-slate-100 hover:font-bold px-4 py-1 rounded-lg self-end transition-all ease-in" onClick={() => handleItemSelect(userInput)}>POST</button>
+                <button className="text-black bg-slate-300 hover:bg-slate-100 hover:font-bold px-4 py-1 rounded-lg self-end transition-all ease-in" onClick={() => handleUpdateGoal()}>POST</button>
             </div>
         </Dialog>
     );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import Heading from "../../components/utilities/Heading";
@@ -11,8 +11,20 @@ import addIcon from "./add.svg";
 import AddGoalDialog from "../../components/AddGoalDialog";
 
 const Home = () => {
+    let goal1 = { id: 123, desc: "This is a demo goal", checked: false };
+    let goal2 = { id: 124, desc: "This is not a demo goal", checked: true };
+
     const [open, setOpen] = useState(false);
-    const [selectedValue, setSelectedValue] = useState("old value");
+    const [goals, setGoals] = useState([goal1, goal2]);
+
+    // main page has been mounted
+    useEffect(() => {
+        console.log("main page reloads 1st time");
+    }, [])
+
+    useEffect(() => {
+        console.log("main page reloads");
+    })
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -20,8 +32,13 @@ const Home = () => {
 
     const handleClose = (value) => {
         setOpen(false);
-        setSelectedValue(value);
+        if (value) {
+            console.log("new goal added");
+            console.log(value);
+        }
     };
+
+    // const goals = [goal1, goal2];
 
     return (
         <div className="pt-10">
@@ -30,21 +47,23 @@ const Home = () => {
                 <CountDownBoxContainer />
             </div>
 
-            {/* <TextContent>
-                Until the preset goals are released.<br />Till then keep hustling!
-            </TextContent> */}
+            {(!goals || goals.length === 0) ? (
+                <TextContent>
+                    Until the preset goals are released.<br />Till then keep hustling!
+                </TextContent>
+            ) : (
+                <div className="goalsContainer w-7/12 mx-auto flex flex-col mt-20">
+                    {goals.map((goal, ind) => {
+                        return (
+                            <GoalsBox key={ind} goal={goal} ind={ind}></GoalsBox>
+                        )
+                    })}
 
-            <div className="goalsContainer w-7/12 mx-auto flex flex-col mt-20">
-                <GoalsBox>Update the goal UI Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, necessitatibus.<br /> dhihsi</GoalsBox>
-
-                <GoalsBox>adipisicing elit. Omnis, necessitatibus. dhihsi</GoalsBox>
-
-                <div className="goal flex items-start my-4" onClick={handleClickOpen}>
-                    <img src={addIcon} alt="add new goal icon" className="w-7 h-7 mt-1" />
+                    <div className="goal flex items-start my-4" onClick={handleClickOpen}>
+                        <img src={addIcon} alt="add new goal icon" className="w-7 h-7 mt-1" />
+                    </div>
                 </div>
-            </div>
-
-            {selectedValue}
+            )}
 
             <div className="fixed bottom-4 right-4 w-5">
                 <Link to={"/broken"}>
@@ -52,7 +71,7 @@ const Home = () => {
                 </Link>
             </div>
 
-            <AddGoalDialog onClose={handleClose} open={open} selectedValue={selectedValue} />
+            <AddGoalDialog onClose={handleClose} open={open} />
         </div>
     );
 };
