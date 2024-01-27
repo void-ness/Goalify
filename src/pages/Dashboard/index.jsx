ac3eb28111514f6aabb3a0d799837f4b95d51406
void-ness <78547746+void-ness@users.noreply.github.com>
2024-01-28 03:26:36 +0530

    update: logout logic

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 
 import Heading from "../../components/utilities/Heading";
@@ -16,26 +16,38 @@ import {
     handleGoalUpdation,
     handleGoalDeletion,
     renderShareLink,
-    handleGoalPositionUpdation
+    handleGoalPositionUpdation,
+    handleLogout
 } from "../../utils/uiHelper";
 
 import { isValidUser } from "../../utils/auth";
-import { AUTH_TOKEN } from "../../constants";
 import LinkDialog from "../../components/LinkDialog";
 
 const Dashboard = () => {
     const [open, setOpen] = useState(false);
     const [goals, setGoals] = useState("");
+    const [loading, setLoading] = useState(true);
     const [linkDialogOpen, setLinkDialogOpen] = useState(false);
     const [shareLink, setShareLink] = useState("");
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (!isValidUser()) {
-            navigate('/broken');
-            return;
-        }
-        renderGoals(setGoals);
+        isValidUser()
+            .then((response) => {
+                if (response.ok) {
+                    renderGoals(setGoals);
+                }
+                else {
+                    navigate('/broken');
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+                navigate('/broken');
+            })
+            .finally(() => {
+                setLoading(false);
+            })
     }, [])
 
     const handleClickOpen = () => {
@@ -49,7 +61,7 @@ const Dashboard = () => {
             return;
         }
 
-        if (source.index == destination.index) {
+        if (source.index === destination.index) {
             return;
         }
 
@@ -64,78 +76,83 @@ const Dashboard = () => {
     }
 
     return (
-        <div className="pt-16 pb-20">
-            <div className="countdownContainer">
-                <Heading>Countdown</Heading>
-                <CountDownBoxContainer />
-            </div>
+        <>
+            {loading ? (<></>) :
+                (
+                    <div className="pt-16 pb-20">
+                        <div className="countdownContainer">
+                            <Heading>Countdown</Heading>
+                            <CountDownBoxContainer />
+                        </div>
 
-            <DragDropContext onDragEnd={onDragEnd}>
-                <div className="goalsContainer w-5/6 md:w-7/12 mx-auto flex flex-col mt-16">
-                    <Droppable droppableId="goals-container">
-                        {(provided) => (
-                            <div className="flex flex-col"
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                            >
-                                {
-                                    (goals && goals.length !== 0) &&
-                                    (
-                                        goals.map((goal, ind) => {
-                                            return (
-                                                <GoalsBox
-                                                    key={ind}
-                                                    index={ind}
-                                                    goal={goal}
-                                                    updateUserGoal={(goal) => handleGoalUpdation(goal, goals, setGoals)}
-                                                    deleteUserGoal={(id) => handleGoalDeletion(id, setGoals, goals)}
-                                                ></GoalsBox>
-                                            )
-                                        })
+                        <DragDropContext onDragEnd={onDragEnd}>
+                            <div className="goalsContainer w-5/6 md:w-7/12 mx-auto flex flex-col mt-16">
+                                <Droppable droppableId="goals-container">
+                                    {(provided) => (
+                                        <div className="flex flex-col"
+                                            {...provided.droppableProps}
+                                            ref={provided.innerRef}
+                                        >
+                                            {
+                                                (goals && goals.length !== 0) &&
+                                                (
+                                                    goals.map((goal, ind) => {
+                                                        return (
+                                                            <GoalsBox
+                                                                key={ind}
+                                                                index={ind}
+                                                                goal={goal}
+                                                                updateUserGoal={(goal) => handleGoalUpdation(goal, goals, setGoals)}
+                                                                deleteUserGoal={(id) => handleGoalDeletion(id, setGoals, goals)}
+                                                            ></GoalsBox>
+                                                        )
+                                                    })
 
-                                    )
-                                }
+                                                )
+                                            }
 
-                                {
-                                    (goals && goals.length === 0) &&
-                                    (
-                                        <div className="text-3xl md:text-4xl mb-6">
-                                            No goals found. Create your first by clicking the button below
+                                            {
+                                                (goals && goals.length === 0) &&
+                                                (
+                                                    <div className="text-3xl md:text-4xl mb-6">
+                                                        No goals found. Create your first by clicking the button below
+                                                    </div>
+                                                )
+                                            }
+
+                                            {provided.placeholder}
                                         </div>
-                                    )
-                                }
+                                    )}
+                                </Droppable>
 
-                                {provided.placeholder}
+                                <div className="goal flex items-start my-4" onClick={handleClickOpen}>
+                                    <img src={addIcon} alt="add new goal icon" className="w-7 h-7 mt-1" />
+                                </div>
                             </div>
-                        )}
-                    </Droppable>
+                        </DragDropContext>
 
-                    <div className="goal flex items-start my-4" onClick={handleClickOpen}>
-                        <img src={addIcon} alt="add new goal icon" className="w-7 h-7 mt-1" />
+                        <div className="fixed bottom-4 right-4 flex flex-col md:flex-row">
+                            <button
+                                onClick={() => renderShareLink(setShareLink, setLinkDialogOpen, linkDialogOpen)}
+                                className="w-5 mb-4 md:mb-0 md:mr-4"
+                            >
+                                <img className="" alt="broken nav icon" src={NavIcon2}></img>
+                            </button>
+
+                            <button
+                                onClick={() => handleLogout(navigate)}
+                                className="w-5"
+                            >
+                                <img className="" alt="broken nav icon" src={NavIcon}></img>
+                            </button>
+                        </div>
+
+                        <LinkDialog open={linkDialogOpen} link={shareLink} onClose={setLinkDialogOpen} />
+
+                        <AddGoalDialog onClose={(value) => handleGoalAddition(value, setOpen, setGoals, goals)} open={open} />
                     </div>
-                </div>
-            </DragDropContext>
-
-            <div className="fixed bottom-4 right-4 flex flex-col md:flex-row">
-                <button
-                    onClick={() => renderShareLink(setShareLink, setLinkDialogOpen, linkDialogOpen)}
-                    className="w-5 mb-4 md:mb-0 md:mr-4"
-                >
-                    <img className="" alt="broken nav icon" src={NavIcon2}></img>
-                </button>
-
-                <Link to={"/broken"}
-                    onClick={() => localStorage.removeItem(AUTH_TOKEN)}
-                    className="w-5"
-                >
-                    <img className="" alt="broken nav icon" src={NavIcon}></img>
-                </Link>
-            </div>
-
-            <LinkDialog open={linkDialogOpen} link={shareLink} onClose={setLinkDialogOpen} />
-
-            <AddGoalDialog onClose={(value) => handleGoalAddition(value, setOpen, setGoals, goals)} open={open} />
-        </div>
+                )}
+        </>
     );
 };
 
